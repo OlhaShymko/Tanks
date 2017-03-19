0b98e0c91eb4bb13247c0eb10dc56fd46f43a48d
Olha Shymko <olha.shymko.91@gmail.com>
2017-03-19 15:56:40 +0200

    EntityType

---
+++ EntityType.java
@@ -0,0 +1,15 @@
+package com.thebyteguru.game;
+
+/**
+ * Created by Chessmaster on 22.05.16.
+ */
+
+//держит в себе названия наших вещей, которые есть в игре
+public enum EntityType {
+
+     Player;
+
+}
+
+
+
